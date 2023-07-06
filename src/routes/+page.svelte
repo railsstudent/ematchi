d081dbf82bac8b409f05243886743b680142d668
Connie Leung <cleung334@gmail.com>
2023-07-06 20:21:52 +0800

    feat: game grid

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import Game from './Game.svelte';
+    import '../styles.css';
+
+</script>
+
+<Game />
---
+++ src/routes/Square.svelte
@@ -0,0 +1,8 @@
+<div class="square"></div>
+
+
+<style>
+    .square {
+        background: yellow;
+    }
+</style>
