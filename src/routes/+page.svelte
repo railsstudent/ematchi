81ad595ea6e242d0cc4dc8d43a29d45c8866a69b
Connie Leung <cleung334@gmail.com>
2023-07-16 02:02:19 +0800

    feat: add new codes to Modal and page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import Game from './Game.svelte';
     import '../styles.css';
 	import Modal from './Modal.svelte';
+    import { levels } from './levels';
 
     let state: 'waiting' | 'playing' | 'paused' | 'won' | 'lost' = 'waiting';
 
@@ -11,8 +12,44 @@
 
 {#if state !== 'playing'}
     <Modal>
-        {#if state === 'waiting'}
+        <header>
+            <h1>e<span>match</span>i</h1>
+            <p>the emoji matching game</p>
+        </header>
+
+        {#if state === 'won' || state === 'lost'} 
+            <p>You {state} the game!</p>
+        {:else if state === 'paused'}
+            <p>Game paused</p>
+        {:else if state === 'waiting'}
             <p>Choose a level</p>
         {/if}
+
+        <div class="buttons">
+            {#if state === 'paused'}
+                <button>resume</button>
+                <button>quit</button>
+            {:else}
+                {#each levels as level}
+                    <button>{level.label}</button>
+                {/each}
+            {/if}
+
+        </div>
     </Modal>
 {/if}
+
+
+<style>
+    h1 {
+        font-size: 4em;
+    }
+
+    h1 span {
+        color: purple;
+    }
+
+    p {
+        font-family: 'Grandstander';
+    }
+</style>
